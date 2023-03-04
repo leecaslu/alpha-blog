6a3f75dd26e1625a468720386221884924a79057
leecaslu <lucashollerlee@gmail.com>
2023-03-04 19:22:24 -0300

    configuring the articles route

---
+++ app/models/article.rb
@@ -0,0 +1,6 @@
+# criando um modelo
+class Article < ApplicationRecord
+  # validators guarantee that the parameters are formated correctly
+  validates :title, presence: true, length: {minimum: 6, maximum: 100}
+  validates :description, presence: true, length: {minimum: 20, maximum: 500}
+end
