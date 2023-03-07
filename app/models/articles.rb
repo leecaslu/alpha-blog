1188174a5e3d1bb16456b8340052bcde7cefcd37
leecaslu <lucashollerlee@gmail.com>
2023-03-07 07:17:11 -0300

    configurando os articles

--- app/models/articles.rb
+++ app/models/articles.rb
@@ -1,5 +1,5 @@
 # criando um modelo
-class Article < ApplicationRecord
+class Articles < ApplicationRecord
   # validators guarantee that the parameters are formated correctly
   validates :title, presence: true, length: {minimum: 6, maximum: 100}
   validates :description, presence: true, length: {minimum: 20, maximum: 500}
