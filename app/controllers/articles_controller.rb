dbc15728b9fc45eb777993cb71fbbab6576c6cd7
leecaslu <lucashollerlee@gmail.com>
2023-03-15 19:44:29 -0300

    configuring links layouts and DRYing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def index
@@ -13,7 +13,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article created successfully"
       redirect_to "/articles/#{@article.id}"
@@ -23,12 +23,10 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
       flash[:notice] = "Article edited successfully"
       redirect_to "/articles/#{@article.id}"
     else
@@ -37,7 +35,6 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     if @article.destroy
       flash[:notice] = "Article deleted successfully"
       redirect_to articles_path
@@ -45,6 +42,17 @@ class ArticlesController < ApplicationController
       flash[:notice] = "Failed to delete the article."
       redirect_to articles_path
     end
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+
+  def article_params
+    params.require(:article).permit(:title, :description)
   end
 
 end
